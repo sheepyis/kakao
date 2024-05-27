1c793eb87216859b07ff71a4dbb8076fc35a9f9c
sheepyis <zx9086@naver.com>
2024-05-27 21:17:14 +0900

    👽week10_kakao link

--- kakao/src/api/ShareKakao.jsx
+++ kakao/src/api/ShareKakao.jsx
@@ -34,18 +34,18 @@ const ShareKakao = () => {
         content: {
             title: '영화 검색 사이트', // 카카오톡 제목
             description: '#UMC #6기 #한성대 #Web #10주차',  // 카카오톡 상세 설명
-            imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-ei3HfkIzsdcQqamoePEOS48-kG0CGesM3Mjv6yxxg&s',    // 카카오톡 이미지
+            imageUrl:'https://pbs.twimg.com/profile_images/1212031261297930241/p6kIo01N_400x400.jpg',    // 카카오톡 이미지
             link: {
-            mobileWebUrl: 'https://hansung-kakao.netlify.app/', // 배포 링크
-            webUrl: 'https://hansung-kakao.netlify.app/',   // 배포 링크
+            mobileWebUrl: 'https://hansung-share.netlify.app/', // 배포 링크
+            webUrl: 'https://hansung-share.netlify.app/',   // 배포 링크
             },
         },
         buttons: [
             {
             title: '영화 찾아보러 가기',    // 카카오톡 버튼
             link: {
-                mobileWebUrl: 'https://hansung-kakao.netlify.app/', // 배포 링크
-                webUrl: 'https://hansung-kakao.netlify.app/',   // 배포 링크
+                mobileWebUrl: 'https://hansung-share.netlify.app/', // 배포 링크
+                webUrl: 'https://hansung-share.netlify.app/',   // 배포 링크
             },
             },
         ],
