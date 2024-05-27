0cc2852dde712e75c71d175ff38c4f0ea56dc5c3
sheepyis <zx9086@naver.com>
2024-05-27 18:07:28 +0900

    🚑week10_login netlify

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -32,6 +32,7 @@ function App() {
           <Route path="/up" element={<UpComing/>} />
           <Route path="/movie/:id" element={<MovieDetailPage/>} />
           <Route path="/login/auth" element={<LoginPage />} />
+          <Route path="/oauth/kakao/callback" element={<LoginPage />} />
           <Route path="*" element={<NotFoundPage/>} />
         </Routes>
         <Footer/>
